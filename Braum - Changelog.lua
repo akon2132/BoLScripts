015d0aaf2676ebfb65d96fe6c951465f2144eb93
QQQ <dnyficaiton@web.de>
2014-05-26 17:38:17 +0200

    Security Update

---
+++ Braum - Changelog.lua
@@ -0,0 +1,10 @@
+Braum - Poro Barber by [Member='QQQ'] and [Member='AWA'] v1.01
+
+-- 1.00 --
+- First Release
+-- 1.01 --
+- Reworked the whole Autojump/Shield logic - should be a lot more responsive now
+- Bugfixes:
+	a. Fixed a bug that spammed an error if you enabled the drawing of the W-Range
+	b. Fixed a bug that spammed an error and caused LaneClear not to work with the inbuild orbwalker
+	c. Fixed a bug that spammed an error if AutoIgnite was enabled and prevented to cast it on an enemy
