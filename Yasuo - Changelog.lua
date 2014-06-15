840f9267232e73c45df8e45574c6a39d3f38216d
QQQ <dnyficaiton@web.de>
2014-06-15 03:21:53 +0200

    v1.01
    -- 1.01 --
    - Added two new Autolevelsequences QEWQ R>Q>E>W and EQWQ R>Q>E>W
    - Bugfixes:
    a. Fixed a bug that caused a spam in the chat with things like
    CastSpell(unsinged int)
    b. You won't walk randomly to targets or lanes anymore (was cause by
    killstealfunction)
    c. More fixes AWA did (don't have a list yet - will be added later into
    the changelog)

--- Yasuo - Changelog.lua
+++ Yasuo - Changelog.lua
@@ -2,3 +2,9 @@ Yasuo - The Windwalker by AWA and QQQ
 
 -- 1.00 --
 - First release
+-- 1.01 --
+- Added two new Autolevelsequences QEWQ R>Q>E>W and EQWQ R>Q>E>W
+- Bugfixes:
+	a. Fixed a bug that caused a spam in the chat with things like CastSpell(unsinged int)
+	b. You won't walk randomly to targets or lanes anymore (was cause by killstealfunction)
+	c. More fixes AWA did (don't have a list yet - will be added later into the changelog)
