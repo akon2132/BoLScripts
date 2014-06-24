3643e4b1e4af0017c1c543bd3b4db01fc252d450
QQQ <dnyfication@web.de>
2014-06-24 21:15:48 +0200

    v2.04
    -- 2.04 --
    - Improved the Hitchance and Collision for spearollision for spears
    - Changes to the Spells (Cougars
    - Changes to the Spells (Cougar W):
    a. Added a toggle to check the minimum distance to jump (standard =
    enabled)
    b. Added a slider to set the range that it needs to your target to jump
    (0-350 - standard = 200)
    - Bugfixes:
    a. Fixed a bug where Nidalee switched from cougar to human even if the
    manacheck was enabled and your mana was low

--- Nidalee - Changelog.lua
+++ Nidalee - Changelog.lua
@@ -228,3 +228,12 @@ Changelog:
 -- 2.03 --
 - Bugfixes:
 	a. Fixed an errorspam with 'string "LIB_PATH = package.path:gsub("?.lua", ")..."]:11 attempt to index local 'p1' (a nil value)'
+-- 2.04 --
+- Improved the Hitchance and Collision for spearollision for spears
+- Changes to the Spells (Cougars
+- Changes to the Spells (Cougar W):
+	a. Added a toggle to check the minimum distance to jump (standard = enabled)
+	b. Added a slider to set the range that it needs to your target to jump (0-350 - standard = 200) 
+- Bugfixes:
+	a. Fixed a bug where Nidalee switched from cougar to human even if the manacheck was enabled and your mana was low
+	
